b7a0a14da59a540653c998d34b39dea8de2f0d5f
TwistedUmbrella <twistedumbrella@gmail.com>
2014-02-10 12:53:35 -0500

    Import tegra11_android_defconfig as basis for making edits

--- buildImg/img.sh
+++ buildImg/img.sh
@@ -8,7 +8,5 @@ BUILDSTRUCT=darwin
 
 fi
 
-RAMDISK="boot.img-ramdisk"
-
-$BUILDSTRUCT/./mkbootfs $RAMDISK | gzip > newramdisk.cpio.gz
-$BUILDSTRUCT/./mkbootimg --cmdline 'no_console_suspend=1 mms_ts.panel_id=18' --kernel zImage --ramdisk newramdisk.cpio.gz -o $3.img
+$BUILDSTRUCT/./mkbootfs boot.img-ramdisk | gzip > newramdisk.cpio.gz
+$BUILDSTRUCT/./mkbootimg --cmdline 'no_console_suspend=1 mms_ts.panel_id=18' --kernel zImage --ramdisk newramdisk.cpio.gz -o boot.img
